f6edc59b8d135d9d066827f65c20c35ae15a530c
brianc725 <brianc725@gmail.com>
2018-02-15 20:18:44 -0800

    Added setup code for Firebase integration

--- app/src/main/java/hackerbois/bruinstrooms/MainActivity.java
+++ app/src/main/java/hackerbois/bruinstrooms/MainActivity.java
@@ -3,11 +3,22 @@ package hackerbois.bruinstrooms;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize database reference
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //testing write message to database
+        mDatabase.setValue("Hello, World!"); //TODO: delete this later
     }
 }
