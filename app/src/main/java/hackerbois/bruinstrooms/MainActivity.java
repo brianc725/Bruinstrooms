ddbf75dd7c2fabed5fb526fc3fea65307e169521
brianc725 <brianc725@gmail.com>
2018-02-26 10:32:24 -0800

    Removed excess import calls

--- app/src/main/java/hackerbois/bruinstrooms/MainActivity.java
+++ app/src/main/java/hackerbois/bruinstrooms/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.firebase.database.ChildEventListener;
@@ -15,7 +14,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
