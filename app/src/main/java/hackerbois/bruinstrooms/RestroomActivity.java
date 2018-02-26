fe043d103e994fc59688a1ab68356c334d5b30f1
brianc725 <brianc725@gmail.com>
2018-02-26 10:33:28 -0800

    Extracted restroom class directly to access getters directly instead of .getValue call

--- app/src/main/java/hackerbois/bruinstrooms/RestroomActivity.java
+++ app/src/main/java/hackerbois/bruinstrooms/RestroomActivity.java
@@ -12,10 +12,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
 public class RestroomActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
@@ -45,9 +41,17 @@ public class RestroomActivity extends AppCompatActivity {
         thisRestroom.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.child("name").getValue(String.class);
-                String gender = dataSnapshot.child("gender").getValue(String.class);
-                String addon;
+                Restroom rest = dataSnapshot.getValue(Restroom.class); //grab our Restroom object
+                String urinals = rest.getUrinals();
+                String name = rest.getName();
+                String hasHandicap = rest.getHasHandicap();  //get all the features from the clas
+                String mirrors = rest.getMirrors();
+                String sinks = rest.getSinks();
+                String stalls = rest.getStalls();
+                String urinalDividers = rest.getUrinalDividers();
+
+                String gender = rest.getGender();
+                String addon;                       //for labeling which gender restroom it is
                 if (gender.equals("male")) {
                     addon = " - MALE";
                 }
@@ -61,16 +65,9 @@ public class RestroomActivity extends AppCompatActivity {
                 title.setText(name + addon);
 
                 TextView features = (TextView) findViewById(R.id.RestroomFeatures);
-                String urinals = dataSnapshot.child("urinals").getValue(String.class);
-
-                String hasHandicap = dataSnapshot.child("hasHandicap").getValue(String.class);
-                String mirrors = dataSnapshot.child("mirrors").getValue(String.class);
-                String sinks = dataSnapshot.child("sinks").getValue(String.class);
-                String stalls = dataSnapshot.child("stalls").getValue(String.class);
-                String urinalDividers = dataSnapshot.child("urinalDividers").getValue(String.class);
 
                 String message = "Urinals: " + urinals + "\nUrinal Dividers: " + urinalDividers + "\nStalls: " + stalls + "\nHandicap Accesible: " + hasHandicap + "\n Sinks: " + sinks+ "\nMirrors: " + mirrors;
-                features.setText(message);
+                features.setText(message); //set the new text
             }
 
             @Override
